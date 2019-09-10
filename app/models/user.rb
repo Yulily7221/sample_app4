bffd7e6be5e4af6862c3bde7521cf5148ff7ab5e
YURISA IKAWA <yurisa19861227@gmail.com>
2019-09-10 14:31:07 +0000

    Sep, 10th

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -33,3 +34,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 end
+
+def feed
+    Micropost.where("user_id = ?", id)
+end
+
+private
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  #get 'static_pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
